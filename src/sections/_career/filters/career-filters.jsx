cf78098d37a3e1287209345e889cff85e2e0bf5f
steeven-js <jacques.steeven@gmail.com>
2024-03-08 20:25:41 -0400

    Mise à jour des filtres de carrière

--- src/sections/_career/filters/career-filters.jsx
+++ src/sections/_career/filters/career-filters.jsx
@@ -10,24 +10,14 @@ import { useResponsive } from 'src/hooks/use-responsive';
 
 import Iconify from 'src/components/iconify';
 
-// import FilterType from './filter-type';
-// import FilterLevel from './filter-level';
-// import FilterSalary from './filter-salary';
-import FilterKeyword from './filter-keyword';
-// import FilterBenefits from './filter-benefits';
-// import FilterLocation from './filter-location';
+import FilterPlantes from './filter-plantes';
 import FilterCategories from './filter-categories';
 
 // ----------------------------------------------------------------------
 
 const defaultValues = {
-  filterKeyword: null,
+  filterPlantes: null,
   filterCategories: null,
-  filterLocation: null,
-  filterType: [],
-  filterLevel: [],
-  filterBenefits: [],
-  filterSalary: [0, 20000],
 };
 
 export default function CareerFilters() {
@@ -37,11 +27,11 @@ export default function CareerFilters() {
 
   const [filters, setFilters] = useState(defaultValues);
 
-  const handleChangeKeyword = useCallback(
+  const handleChangePlantes = useCallback(
     (newValue) => {
       setFilters({
         ...filters,
-        filterKeyword: newValue,
+        filterPlantes: newValue,
       });
     },
     [filters]
@@ -57,66 +47,6 @@ export default function CareerFilters() {
     [filters]
   );
 
-  // const handleChangeLocation = useCallback(
-  //   (newValue) => {
-  //     setFilters({
-  //       ...filters,
-  //       filterLocation: newValue,
-  //     });
-  //   },
-  //   [filters]
-  // );
-
-  // const handleChangeJobType = useCallback(
-  //   (event) => {
-  //     const {
-  //       target: { value },
-  //     } = event;
-
-  //     setFilters({
-  //       ...filters,
-  //       filterType: typeof value === 'string' ? value.split(',') : value,
-  //     });
-  //   },
-  //   [filters]
-  // );
-
-  // const handleChangeJobLevel = useCallback(
-  //   (event) => {
-  //     const {
-  //       target: { value },
-  //     } = event;
-  //     setFilters({
-  //       ...filters,
-  //       filterLevel: typeof value === 'string' ? value.split(',') : value,
-  //     });
-  //   },
-  //   [filters]
-  // );
-
-  // const handleChangeJobBenefits = useCallback(
-  //   (event) => {
-  //     const {
-  //       target: { value },
-  //     } = event;
-  //     setFilters({
-  //       ...filters,
-  //       filterBenefits: typeof value === 'string' ? value.split(',') : value,
-  //     });
-  //   },
-  //   [filters]
-  // );
-
-  // const handleChangeSalary = useCallback(
-  //   (event, newValue) => {
-  //     setFilters({
-  //       ...filters,
-  //       filterSalary: newValue,
-  //     });
-  //   },
-  //   [filters]
-  // );
-
   const onReset = useCallback(() => {
     setFilters(defaultValues);
   }, []);
@@ -130,20 +60,15 @@ export default function CareerFilters() {
   const renderFilters = (
     <>
       <Stack spacing={2.5} direction={{ xs: 'column', md: 'row' }} alignItems="center">
-        <FilterKeyword
-          filterKeyword={filters.filterKeyword}
-          onChangeKeyword={handleChangeKeyword}
+        <FilterPlantes
+          filterPlantes={filters.filterPlantes}
+          onChangePlantes={handleChangePlantes}
         />
 
         <FilterCategories
           filterCategories={filters.filterCategories}
           onChangeCategory={handleChangeCategory}
         />
-
-        {/* <FilterLocation
-          filterLocation={filters.filterLocation}
-          onChangeLocation={handleChangeLocation}
-        /> */}
 
         {mdUp && (
           <Button
@@ -157,19 +82,6 @@ export default function CareerFilters() {
           </Button>
         )}
       </Stack>
-
-      {/* <Stack direction={{ xs: 'column', md: 'row' }} spacing={{ xs: 2.5, md: 1 }} sx={{ mt: 2.5 }}>
-        <FilterType filterType={filters.filterType} onChangeJobType={handleChangeJobType} />
-
-        <FilterLevel filterLevel={filters.filterLevel} onChangeJobType={handleChangeJobLevel} />
-
-        <FilterSalary filterSalary={filters.filterSalary} onChangeSalary={handleChangeSalary} />
-
-        <FilterBenefits
-          filterBenefits={filters.filterBenefits}
-          onChangeJobBenefits={handleChangeJobBenefits}
-        />
-      </Stack> */}
 
       {!mdUp && (
         <Button
